3328d05776a08eb0fc928f00e7d411c476f929fd
Waqar Amin <vacaramin86@gmail.com>
2024-03-05 02:53:22 +0500

    Project Section removed

--- src/index.js
+++ src/index.js
@@ -7,14 +7,12 @@ import "bootstrap/scss/bootstrap.scss";
 import "assets/scss/paper-kit.scss?v=1.3.0";
 // pages
 import Index from "views/Index.js";
-import projectSection from "components/Sections/projectSection";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/Persona" element={<Index />} />
-      <Route path="/Projects" element={<projectSection />} />
       
       <Route path="/*" element={<Navigate to="/Persona" replace />} />
     </Routes>
