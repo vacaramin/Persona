453084cf85ba07e037fe4c58360bc1c8e9465f1b
Waqar Amin <vacaramin86@gmail.com>
2024-03-18 04:05:57 +0500

    removed sections

--- src/components/Headers/IndexHeader.js
+++ src/components/Headers/IndexHeader.js
@@ -43,40 +43,7 @@ function IndexHeader() {
       >
         <div className="filter" />
         <div className="content-center">
-          <Container>
-            <div className="title-brand">
-              <img
-                alt="..."
-                className="img-circle img-responsive img-raised title-image"
-                width={"35%"}
-                src={require("assets/img/faces/vacaramin.png")}
-                style={{
-                  transform: `scale(${1 - scrollY * 0.002})`,
-                }}
-              />
-
-              <h1
-                className="presentation-title"
-                style={{
-                  transform: `scale(${1 - scrollY * 0.002})`,
-                  opacity:1
-                }}
-              >
-                Full-Stack Engineer
-              </h1>
-
-              <div className="fog-low">
-                <img alt="..." src={require("assets/img/fog-low.png")} style={{ zIndex: "10px",opacity: darkmode? 1 : 0.7, }} />
-              </div>
-              <div className="fog-low right" style={{ zIndex: "10px" }}>
-                <img alt="..." src={require("assets/img/fog-low.png")} style={{ zIndex: "10px",opacity: darkmode? 1 : 0.7, }} />
-              </div>
-            </div>
-
-            <h2 className="presentation-subtitle text-center" style={{ position: "relative", bottom: "10px", transform: `scale(${1 - scrollY * 0.002})` }}>
-              Code Craftsman | Software Designer | Devops Engineer
-            </h2>
-          </Container>
+          
         </div>
         <div
           className="moving-clouds"
