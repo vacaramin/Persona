60268c03e8625f667c23a01e4097379590f1cb80
Waqar Amin <vacaramin86@gmail.com>
2024-03-17 21:45:35 +0500

    removal of the scroll

--- src/components/Headers/IndexHeader.js
+++ src/components/Headers/IndexHeader.js
@@ -87,19 +87,7 @@ function IndexHeader() {
         />
       </div>
 
-      <div style={{ height: "2000px"}}>
-        {/* <MainSection/> */}
-        <h1
-                className="presentation-title"
-                style={{
-                  opacity:1,
-                  position:"inherit",
-                  top:"9px"
-                }}
-              >
-                Website Under Construction
-              </h1>
-      </div>
+      
       </div>
     </>
   );
